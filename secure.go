d2e9a57a3dcf120ad468f72556b591b836a398d0
Franklin De Los Santos <franklin.delossantos@flumehealth.com>
2022-07-05 14:22:55 -0700

    check if IsHostAllowed is set

--- secure.go
+++ secure.go
@@ -329,7 +329,13 @@ func (s *Secure) processRequest(w http.ResponseWriter, r *http.Request) (http.He
 				}
 			}
 		}
-		if !isGoodHost && !s.opt.IsHostAllowed(r.Host) {
+
+		var isHostAllowed bool
+		if s.opt.IsHostAllowed != nil {
+			isHostAllowed = s.opt.IsHostAllowed(r.Host)
+		}
+
+		if !isGoodHost && !isHostAllowed {
 			s.badHostHandler.ServeHTTP(w, r)
 			return nil, nil, fmt.Errorf("bad host name: %s", host)
 		}
